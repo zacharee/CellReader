6fc52387d2648aefabb3a709be29731a0fdb20c8
Zachary Wander <zachary.wander@gmail.com>
2022-01-04 13:30:18 -0500

    Redo arrow

--- app/src/main/java/dev/zwander/cellreader/ui/components/Indenticator.kt
+++ app/src/main/java/dev/zwander/cellreader/ui/components/Indenticator.kt
@@ -1,36 +1,46 @@
 package dev.zwander.cellreader.ui.components
 
-import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import dev.zwander.cellreader.R
-import dev.zwander.cellreader.utils.asDp
 
 @Composable
-fun BoxScope.IndenticatorLine(size: IntSize, isFinal: Boolean) {
-    Box(
+fun IndenticatorLine(
+    size: IntSize,
+    isFinal: Boolean,
+) {
+    val density = LocalDensity.current
+
+    Canvas(
         modifier = Modifier
-            .height(size.height.asDp())
-            .width(16.dp)
-            .padding(start = 13.503.dp, bottom = if (isFinal) (size.height / 2f + 13).asDp() else 0.dp)
-            .background(Color.White, RoundedCornerShape(
-                topStart = 1.25.dp,
-                topEnd = 1.25.dp,
-                bottomStart = if (isFinal) 0.dp else 1.25.dp,
-                bottomEnd = if (isFinal) 0.dp else 1.25.dp
-            ))
-            .align(Alignment.TopStart)
-            .animateContentSize()
-    )
+            .padding(start = 9.5.dp)
+            .width(3.dp)
+            .height((size.height / density.density).dp),
+    ) {
+        drawLine(
+            brush = SolidColor(Color.White),
+            cap = StrokeCap.Round,
+            start = Offset.Zero,
+            end = Offset(
+                0f,
+                if (isFinal) (size.height / 2f - (7.5f * density.density)) else size.height.toFloat()
+            ),
+            strokeWidth = drawContext.size.width
+        )
+    }
 }
 
 @Composable
@@ -39,8 +49,10 @@ fun IndenticatorArrow() {
         painter = painterResource(id = R.drawable.indent_arrow),
         contentDescription = null,
         modifier = Modifier
-            .width(40.dp)
-            .height(32.dp)
-            .padding(start = 8.dp)
+            .padding(start = 8.dp, end = 4.dp)
+            .width(24.dp)
+            .height(24.dp),
+        contentScale = ContentScale.Fit,
+        alignment = Alignment.CenterStart
     )
 }
