1b0003b8b8c4908fc5da3bc82389c9eba5ccd459
Zachary Wander <zachary.wander@gmail.com>
2022-01-23 17:26:06 -0500

    Experiment with putting type indicator next to signal bars

--- app/src/main/java/dev/zwander/cellreader/ui/components/LevelIndicator.kt
+++ app/src/main/java/dev/zwander/cellreader/ui/components/LevelIndicator.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -14,28 +15,40 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.zwander.cellreader.R
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 @Composable
-fun LevelIndicator(level: Int, dBm: Int) {
+fun LevelIndicator(level: Int, dBm: Int, type: String) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(
-                when (level) {
-                    CellSignalStrength.SIGNAL_STRENGTH_POOR -> R.drawable.cell_1
-                    CellSignalStrength.SIGNAL_STRENGTH_MODERATE -> R.drawable.cell_2
-                    CellSignalStrength.SIGNAL_STRENGTH_GOOD -> R.drawable.cell_3
-                    CellSignalStrength.SIGNAL_STRENGTH_GREAT -> R.drawable.cell_4
-                    else -> R.drawable.cell_0
-                }
-            ),
-            contentDescription = null,
-            modifier = Modifier
-                .width(32.dp)
-                .height(32.dp),
-            contentScale = ContentScale.Fit
-        )
+        Box {
+            Text(
+                text = type.first().toString(),
+                textAlign = TextAlign.Center,
+                fontSize = 14.sp,
+                modifier = Modifier.align(Alignment.TopCenter)
+                    .padding(end = 16.dp)
+            )
+
+            Image(
+                painter = painterResource(
+                    when (level) {
+                        CellSignalStrength.SIGNAL_STRENGTH_POOR -> R.drawable.cell_1
+                        CellSignalStrength.SIGNAL_STRENGTH_MODERATE -> R.drawable.cell_2
+                        CellSignalStrength.SIGNAL_STRENGTH_GOOD -> R.drawable.cell_3
+                        CellSignalStrength.SIGNAL_STRENGTH_GREAT -> R.drawable.cell_4
+                        else -> R.drawable.cell_0
+                    }
+                ),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(32.dp)
+                    .height(32.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
 
         Text(
             text = "$dBm dBm",
