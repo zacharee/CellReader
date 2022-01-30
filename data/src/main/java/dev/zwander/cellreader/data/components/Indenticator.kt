9533f14d555a4e40f48d47aca4c22fde3e39226d
Zachary Wander <zachary.wander@gmail.com>
2022-01-30 08:52:09 -0500

    Make indenticator smaller on wear

--- data/src/main/java/dev/zwander/cellreader/data/components/Indenticator.kt
+++ data/src/main/java/dev/zwander/cellreader/data/components/Indenticator.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -11,11 +12,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import dev.zwander.cellreader.data.R
+import dev.zwander.cellreader.data.util.isWear
 
 @Composable
 fun IndenticatorLine(
@@ -23,11 +26,19 @@ fun IndenticatorLine(
     isFinal: Boolean,
 ) {
     val density = LocalDensity.current
+    val context = LocalContext.current
+
+    val isWear = remember {
+        context.isWear
+    }
+
+    val lineWidth = if (isWear) 2 else 3
+    val finalFactor = if (isWear) 5.5f else 7.5f
 
     Canvas(
         modifier = Modifier
-            .padding(start = 1.5.dp)
-            .width(3.dp)
+            .padding(start = (lineWidth / 2f).dp)
+            .width(lineWidth.dp)
             .height((size.height / density.density).dp),
     ) {
         drawLine(
@@ -36,7 +47,7 @@ fun IndenticatorLine(
             start = Offset.Zero,
             end = Offset(
                 0f,
-                if (isFinal) (size.height / 2f - (7.5f * density.density)) else size.height.toFloat()
+                if (isFinal) (size.height / 2f - (finalFactor * density.density)) else size.height.toFloat()
             ),
             strokeWidth = drawContext.size.width
         )
@@ -45,13 +56,18 @@ fun IndenticatorLine(
 
 @Composable
 fun IndenticatorArrow() {
+    val context = LocalContext.current
+
+    val isWear = remember {
+        context.isWear
+    }
+
     Image(
         painter = painterResource(id = R.drawable.indent_arrow),
         contentDescription = null,
         modifier = Modifier
             .padding(start = 0.dp, end = 4.dp)
-            .width(24.dp)
-            .height(24.dp),
+            .size(if (isWear) 16.dp else 24.dp),
         contentScale = ContentScale.Fit,
         alignment = Alignment.CenterStart
     )
