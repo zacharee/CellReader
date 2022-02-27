b644df6ea21256041e1027c318eda0c502b21021
Zachary Wander <zachary.wander@gmail.com>
2022-02-27 10:33:02 -0500

    Remove unused import

--- data/src/main/java/dev/zwander/cellreader/data/components/CardCheckbox.kt
+++ data/src/main/java/dev/zwander/cellreader/data/components/CardCheckbox.kt
@@ -10,7 +10,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterialApi::class)
