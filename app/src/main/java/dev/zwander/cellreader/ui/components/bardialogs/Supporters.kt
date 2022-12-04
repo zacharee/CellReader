7758915cf62c0e68cff65b2ba6d6b0b378280678
Zachary Wander <zachary.wander@gmail.com>
2022-12-03 20:20:53 -0500

    Add MDY icon

--- app/src/main/java/dev/zwander/cellreader/ui/components/bardialogs/Supporters.kt
+++ app/src/main/java/dev/zwander/cellreader/ui/components/bardialogs/Supporters.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import dev.zwander.cellreader.data.components.WearSafeText
@@ -37,8 +39,9 @@ fun Supporters() {
         contentPadding = PaddingValues(8.dp)
     ) {
         itemsIndexed(supporters, { _, item -> item.hashCode() }) { _, item ->
-            Card(
-                modifier = Modifier.fillMaxWidth()
+            OutlinedCard(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.outlinedCardColors(containerColor = Color.Transparent)
             ) {
                 Box(
                     modifier = Modifier
